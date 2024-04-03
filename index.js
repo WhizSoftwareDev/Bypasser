2e16358771f5ff0d91e6c22e2cac24c6eeec7848
WhizSoftware <97331713+WhizSoftwareDev@users.noreply.github.com>
2024-04-04 01:24:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const domain = window.location.hostname;
+const domainList = [];
+
+
 if(domain === "ouo.press"){
     const Button = document.querySelector("#btn-main");
     if(Button){
         Button.click();
+    }
+}
+for(var i = 0; i <domainList.Length; i++){
+    if(domain === domainList[i]){
+        window.location.href = window.conf_rew.link;
     }
 }
