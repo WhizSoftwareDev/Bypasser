84a72157af69a9eb9bb52198eb50b0e33f4a66c5
WhizSoftware <97331713+WhizSoftwareDev@users.noreply.github.com>
2024-04-04 01:19:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-alert(window.location.hostname);
-const Button = document.querySelector("#btn-main");
-if(Button){
-    Button.click();
+const domain = window.location.hostname;
+if(domain === "ouo.press"){
+    const Button = document.querySelector("#btn-main");
+    if(Button){
+        Button.click();
+    }
 }
